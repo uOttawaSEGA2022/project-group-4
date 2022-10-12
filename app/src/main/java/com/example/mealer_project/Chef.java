a2083023f4c8edbffa1b40e577a80f05b974b459
Rahul <Ratre075@uottawa.ca>
2022-10-12 18:22:57 -0400

    Added classes for Admin, Chef, and Meal

---
+++ app/src/main/java/com/example/mealer_project/Chef.java
@@ -0,0 +1,128 @@
+package com.example.mealer_project;
+
+import androidx.annotation.NonNull;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ * This class instantiates an instance of Chef for Mealer App
+ * Child Class of User
+ */
+public class Chef extends User {
+    private String description;
+    private String voidCheque;
+    private int chefRating;
+    private int numOfMealsSold;
+    private Map<String, Meal> chefMenu;
+    /**
+     * Create a single instance of chef
+     * @param firstName First name of the chef
+     * @param lastName Last name of the chef
+     * @param email email of the chef
+     * @param password password for the chef
+     * @param address address of the chef
+     * @param role Role of the chef
+     * @param description Description of the chef
+     * @param voidCheque Chequing information of chef
+     * Menu of a chef is stored in a HashMap
+     */
+    Chef(String firstName, String lastName, String email, String password, String address,
+         UserRoles role, String description, String voidCheque, int numberOfMealsSold, int chefRating) {
+        // instantiate Admins data members
+        super(firstName, lastName, email, password, address, role);
+        this.setDescription(description);
+        this.setVoidCheque(voidCheque);
+        this.setNumOfMealsSold(numOfMealsSold);
+        this.setChefRating(chefRating);
+        this.chefMenu = new HashMap<String, Meal>(); //<MealID, Meal> key-value pair
+    }
+
+    /**
+     * Get a short description of the chef
+     * @return String representing chef's description
+     */
+    public String getDescription() { return description; }
+
+    /**
+     * Set the chef's description
+     * @param description String representing the chef's description
+     */
+    public void setDescription(String description) {
+        // validate description
+        this.description = description;
+    }
+
+    /**
+     * Get chequing information about chef
+     * @return String representing void cheque of chef
+     */
+    public String getVoidCheque() {
+        return voidCheque;
+    }
+
+    /**
+     * Set the chef's void cheque
+     * @param voidCheque String representing void cheque of chef
+     */
+    public void setVoidCheque(String voidCheque) {
+        // validate void cheque
+        this.voidCheque = voidCheque;
+    }
+
+    /**
+     * Get the average rating of a chef
+     * @return Integer representing chef's overall rating
+     */
+    public int getChefRating() { return chefRating; }
+
+    /**
+     * Set the chef's rating
+     * @param chefRating integer representing the chef's rating
+     */
+    public void setChefRating(int chefRating) {
+        // validate rating
+        this.chefRating = chefRating;
+    }
+
+    /**
+     * Get the total number of meals sold by a chef
+     * @return Integer representing chef's total sales
+     */
+    public int getNumOfMealsSold() { return numOfMealsSold; }
+
+    /**
+     * Set the chef's total meals sold
+     * @param numOfMealsSold integer representing the chef's total sales
+     */
+    public void setNumOfMealsSold(int numOfMealsSold) {
+        // validate num of meals sold
+        this.numOfMealsSold = numOfMealsSold;
+    }
+
+    /**
+     * Retrieve a meal from the chef's menu
+     * @param mealID representing the ID of the meal
+     * @return Meal instance from chef's menu
+     */
+    public Meal getMeal(String mealID) {
+        return this.chefMenu.get(mealID);
+    }
+
+    /**
+     * Add a new meal to the chef's menu
+     * @param newMeal Meal to be added to menu
+     */
+    public void addMeal(@NonNull Meal newMeal) {
+        chefMenu.put(newMeal.getMealID(), newMeal);
+    }
+
+    /**
+     * Remove a meal from the chef's menu
+     * @param removeMeal Meal to be removed from menu
+     */
+    public void removeMeal(@NonNull Meal removeMeal) {
+        chefMenu.remove(removeMeal.getMealID());
+    }
+}
---
+++ app/src/main/java/com/example/mealer_project/Meal.java
@@ -0,0 +1,187 @@
+package com.example.mealer_project;
+
+
+import java.util.Date;
+
+/**
+ * This class is a template/blueprint for each instance of a Meal on a chef's menu
+ */
+public class Meal {
+    private String name;
+    private String mealID;
+    private String chefID;
+    private String cuisineType;
+    private String mealType;
+    private String ingredients;
+    private String allergens;
+    private String description;
+    private boolean offered;
+    private double price;
+    /**
+     * Create an instance of meal
+     * @param name Name of the meal
+     * @param mealID ID number of the meal
+     * @param chefID Chef ID of the meal
+     * @param cuisineType Cuisine Type of the meal (Italian, Chinese, Greek)
+     * @param mealType Meal Type (Main dish, Soup, Desert)
+     * @param ingredients Ingredients used in the meal
+     * @param allergens Potential allergens in meal
+     * @param description Short description of the meal
+     * @param offered Whether the meal is currently offered or not
+     * @param price Current price of the meal
+     */
+    Meal(String name, String mealID, String chefID, String cuisineType, String mealType,
+         String ingredients, String allergens, String description, boolean offered, double price) {
+
+        this.setName(name);
+        this.setMealID(mealID);
+        this.setChefID(chefID);
+        this.setCuisineType(cuisineType);
+        this.setMealType(mealType);
+        this.setIngredients(ingredients);
+        this.setAllergens(allergens);
+        this.setDescription(description);
+        this.setOffered(offered);
+        this.setPrice(price);
+    }
+
+    /**
+     * Get the name of the meal
+     * @return Meal name
+     */
+    public String getName() { return name; }
+
+    /**
+     * Set/Change the name of the meal
+     * @param name Meal's name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get the ID String of the meal
+     * @return Meal's ID (Identification)
+     */
+    public String getMealID() { return mealID; }
+
+    /**
+     * Set/Change the ID of the meal
+     * @param mealID of meal
+     */
+    public void setMealID(String mealID) {
+        this.mealID = mealID;
+    }
+
+    /**
+     * Get the ID of the chef who cooked meal
+     * @return Chef's ID
+     */
+    public String getChefID() { return chefID; }
+
+    /**
+     * Set/Change Chef's ID
+     * @param chefID of meal
+     */
+    public void setChefID(String chefID) {
+        this.chefID = chefID;
+    }
+
+    /**
+     * Get the cuisine type of the meal
+     * @return Meal's Cuisine Type
+     */
+    public String getCuisineType() { return cuisineType; }
+
+    /**
+     * Change Meal's Cuisine Type
+     * @param cuisineType of meal
+     */
+    public void setCuisineType(String cuisineType) {
+        this.cuisineType = cuisineType;
+    }
+
+    /**
+     * Get the type of the meal (Main dish, soup, etc)
+     * @return Meal's type
+     */
+    public String getMealType() { return mealType; }
+
+    /**
+     * Change the type of the meal
+     * @param mealType of meal
+     */
+    public void setMealType(String mealType) {
+        this.mealType = mealType;
+    }
+
+    /**
+     * Get the list of ingredients used in the meal
+     * @return Ingredients used in meal
+     */
+    public String getIngredients() { return ingredients; }
+
+    /**
+     * Set/Change the list of ingredients used in meal
+     * @param ingredients list
+     */
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    /**
+     * Get the list of allergens of the meal
+     * @return Allergen list for meal
+     */
+    public String getAllergens() { return allergens; }
+
+    /**
+     * Set/Change the list of allergens of the meal
+     * @param allergens list
+     */
+    public void setAllergens(String allergens) {
+        this.allergens = allergens;
+    }
+
+    /**
+     * Get a short description of the meal
+     * @return Meal Description
+     */
+    public String getDescription() { return description; }
+
+    /**
+     * Set/Change the description of the meal
+     * @param description of meal
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Check if a meal is currently being offered by a chef or not
+     * @return Boolean representing True if offered, False if not offered
+     */
+    public boolean isOffered() { return offered; }
+
+    /**
+     * Set/Change the state of whether meal is offered or not
+     * @param offered offered or not
+     */
+    public void setOffered(boolean offered) {
+        this.offered = offered;
+    }
+
+    /**
+     * Get the price of the meal offered by chef
+     * @return Meal price
+     */
+    public double getPrice() { return price; }
+
+    /**
+     * Set/Change the price of the meal
+     * @param price Meal Price
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
