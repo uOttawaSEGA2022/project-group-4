36178b427932a048e17efc81eb18626e47e8fe68
Rahul <Ratre075@uottawa.ca>
2022-10-16 18:15:29 -0400

    Added authentication code

--- app/src/main/java/com/example/mealer_project/ui/signup/SignupActivity.java
+++ app/src/main/java/com/example/mealer_project/ui/signup/SignupActivity.java
@@ -1,16 +1,55 @@
 package com.example.mealer_project.ui.signup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.mealer_project.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignupActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        signup("hello123@gmail.com", "123456789");
     }
+
+    private void signup(String email, String password) {
+
+        FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            System.out.println("Successfull");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                        }
+                    }
+                });
+
+
+    }
+
+
+
 }
