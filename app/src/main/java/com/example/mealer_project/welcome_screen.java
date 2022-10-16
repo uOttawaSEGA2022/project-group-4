695da7a1c6a026311f500c52edcfe39719c1c66c
kkristene3 <kkristene3@gmail.com>
2022-10-16 16:06:31 -0400

    Added welcome screen

---
+++ app/src/main/java/com/example/mealer_project/welcome_screen.java
@@ -0,0 +1,14 @@
+package com.example.mealer_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class welcome_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+}
