41fb8bff8fc42625e87ad24cf9c798190a5e0fa7
kkristene3 <kkristene3@gmail.com>
2022-10-16 16:27:06 -0400

    Added code to navigate back to intro screen

--- app/src/main/java/com/example/mealer_project/welcome_screen.java
+++ app/src/main/java/com/example/mealer_project/welcome_screen.java
@@ -2,6 +2,7 @@ package com.example.mealer_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -22,6 +23,8 @@ public class welcome_screen extends AppCompatActivity {
     }
 
     public void clickLogout (View view) {
-        finish(); // change later to proper code
+        Intent intent = new Intent (this, IntroScreen.class);
+        startActivity(intent);
+        //finish(); // change later to proper code
     }
 }
