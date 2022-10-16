94e15d1c930771aed564ca3571c5084064c82648
pranav-kural <pranavkural042@gmail.com>
2022-10-16 04:59:20 -0400

    Completed the sign up component UI

---
+++ app/src/main/java/com/example/mealer_project/ui/signup/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.example.mealer_project.ui.signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mealer_project.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
