1fc35c9013ced8dec8207811df7b1f7adca3eeff
kkristene3 <kkristene3@gmail.com>
2022-10-16 16:11:19 -0400

    Added code for welcome screen

--- app/src/main/java/com/example/mealer_project/welcome_screen.java
+++ app/src/main/java/com/example/mealer_project/welcome_screen.java
@@ -3,6 +3,9 @@ package com.example.mealer_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class welcome_screen extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class welcome_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        // Change text to proper welcome message when opened
+        String role = "wowowowo";
+        String name = "plssss";
+        TextView editText = (TextView) findViewById(R.id.welcome_message);
+        editText.setText("Welcome " + role + " " +  name + "!", TextView.BufferType.EDITABLE);
+    }
+
+    public void clickLogout (View view) {
+        finish(); // change later to proper code
     }
 }
