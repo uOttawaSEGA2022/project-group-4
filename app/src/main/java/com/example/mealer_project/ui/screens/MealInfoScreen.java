7bfbdd4b328f45133515d6aec355e5955bcd2c43
kkristene3 <kkristene3@gmail.com>
2022-11-13 16:47:26 -0500

    Create meal info screen, and fix chef screen

---
+++ app/src/main/java/com/example/mealer_project/ui/screens/MealInfoScreen.java
@@ -0,0 +1,16 @@
+package com.example.mealer_project.ui.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mealer_project.R;
+
+public class MealInfoScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_info_screen);
+    }
+}
