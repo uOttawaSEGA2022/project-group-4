b5f5e89951e9e597e6f2b3582e6eadd54d9c71f9
Anjali Mohammed <amoha424@uottawa.ca>
2022-11-20 14:57:48 -0500

    Made addOrder method

--- app/src/main/java/com/example/mealer_project/data/sources/FirebaseCollections.java
+++ app/src/main/java/com/example/mealer_project/data/sources/FirebaseCollections.java
@@ -7,5 +7,6 @@ public abstract class FirebaseCollections {
     public final static String CHEF_COLLECTION = "Chefs";
     public final static String ADMIN_COLLECTION = "Admin";
     public final static String MEAL_COLLECTION = "Meals";
+    public final static String ORDER_COLLECTION = "Orders";
     public final static String CHEF_MEALS_COLLECTION = "meals";
 }
--- app/src/main/java/com/example/mealer_project/data/sources/actions/OrderActions.java
+++ app/src/main/java/com/example/mealer_project/data/sources/actions/OrderActions.java
@@ -1,6 +1,20 @@
 package com.example.mealer_project.data.sources.actions;
 
+import static com.example.mealer_project.data.handlers.MealHandler.dbOperations.ADD_MEAL;
+import static com.example.mealer_project.data.sources.FirebaseCollections.CHEF_COLLECTION;
+import static com.example.mealer_project.data.sources.FirebaseCollections.MEAL_COLLECTION;
+import static com.example.mealer_project.data.sources.FirebaseCollections.ORDER_COLLECTION;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.example.mealer_project.app.App;
+import com.example.mealer_project.data.models.Order;
 import com.example.mealer_project.utils.Preconditions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -10,23 +24,47 @@ public class OrderActions {
 
     FirebaseFirestore database;
 
+
     public OrderActions(FirebaseFirestore database) {
         this.database = database;
     }
 
 
+    /**
+    * Adds order to firebase
+    * @param order order object to be added
+     */
     public void addOrder(Order order){
 
         if (Preconditions.isNotNull(order)) {
 
             Map<String, Object> databaseOrder = new HashMap<>();
 
-            databaseOrder.put("orderID", order.getID());
+            databaseOrder.put("orderID", order.getOrderID());
             databaseOrder.put("chefID", order.getChefID());
             databaseOrder.put("clientID", order.getClientID());
-            databaseOrder.put("listOfMeals", order.getListOfMeals());
-            databaseOrder.put("pending", order.pending());
+            databaseOrder.put("listOfMeals", order.get());
+            databaseOrder.put("pending", order.getPendingStatus());
             databaseOrder.put("completed", order.getIsCompleted());
-            databaseOrder.put("date", order.getDate());
+            databaseOrder.put("date", order.getOrderDate());
+
+            database.collection(ORDER_COLLECTION)
+                    .document(order.getChefID())
+                    .set(databaseOrder)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            // update meal id
+                            Log.e("MEAL ID", documentReference.getId());
+                            App.MEAL_HANDLER.handleActionSuccess(ADD_MEAL, meal);
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            App.MEAL_HANDLER.handleActionFailure(ADD_MEAL, "Failed to add meal to chef in database: " + e.getMessage());
+                        }
+                    });
     }
 }
