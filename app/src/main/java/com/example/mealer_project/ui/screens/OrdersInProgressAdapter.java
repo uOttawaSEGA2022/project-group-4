067e4e86c1ad4e3cce4ad44c5481f36de5f1b4e1
kkristene3 <kkristene3@gmail.com>
2022-11-23 18:23:26 -0500

    stuff

---
+++ app/src/main/java/com/example/mealer_project/ui/screens/OrdersInProgressAdapter.java
@@ -0,0 +1,52 @@
+package com.example.mealer_project.ui.screens;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.mealer_project.R;
+import com.example.mealer_project.app.App;
+import com.example.mealer_project.data.models.Chef;
+import com.example.mealer_project.data.models.orders.OrderItem;
+
+import java.util.List;
+
+
+public class OrdersInProgressAdapter extends ArrayAdapter<OrderItem> {
+
+
+    /**
+     * Constructor
+     *
+     * @param context
+     * @param resource
+     * @param objects
+     */
+    public OrdersInProgressAdapter(@NonNull Context context, int resource, @NonNull List<OrderItem> objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        // get chef's orders in progress
+        Chef chef = App.getChef();
+        chef.ORDER
+
+
+        // check if an existing view is being reused, otherwise, inflate
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.orders_in_progress, parent, false);
+        }
+
+        // populate data
+
+        return convertView;
+    }
+}
