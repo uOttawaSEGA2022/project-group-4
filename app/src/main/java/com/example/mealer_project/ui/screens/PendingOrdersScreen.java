963b5731ad8bdef37dea415a0106e34850f953cc
Amy Huang <ahuan106@uottawa.ca>
2022-11-23 16:02:54 -0500

    cleaning up the code for the PendingOrdersScreen

--- app/src/main/java/com/example/mealer_project/ui/screens/PendingOrdersScreen.java
+++ app/src/main/java/com/example/mealer_project/ui/screens/PendingOrdersScreen.java
@@ -1,10 +1,7 @@
 package com.example.mealer_project.ui.screens;
 
-import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
@@ -12,7 +9,6 @@ import com.example.mealer_project.R;
 import com.example.mealer_project.app.App;
 import com.example.mealer_project.data.models.Chef;
 import com.example.mealer_project.data.models.Order;
-import com.example.mealer_project.data.models.meals.Meal;
 import com.example.mealer_project.ui.core.UIScreen;
 
 import java.util.ArrayList;
@@ -22,71 +18,87 @@ import java.util.Map;
 
 public class PendingOrdersScreen extends UIScreen {
 
+    // Variable Declaration
+    /**
+     * the map that contains the current CHEF's Orders
+     */
     private Map<String, Order> ordersData;
+
+    /**
+     * the list that will hold the orders
+     */
     private List<Order> orders;
 
+    /**
+     * the array adapter for the list view of the pending orders
+     */
     private PendingOrdersAdapter pendingOrdersAdapter;
 
+    /**
+     * the back button icon
+     */
     ImageButton backButton;
 
+    //----------------------------------------------------------------------------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pending_orders_screen);
 
-        // check the meals data to be loaded
-        loadPendingOrdersData();
-
         // Initialization
         ordersData = new HashMap<String, Order>();
-        backButton = (ImageButton) findViewById(R.id.backButton);
+        backButton = findViewById(R.id.backButton);
+
+        // Process: loading the Orders data
+        loadPendingOrdersData();
+
+        // Process: populating the Orders ListView
+        populatePendingOrdersList();
 
         // Process: setting onClick method for back button
         backButton.setOnClickListener(v -> finish());
 
-        // populate orders listView
-        populatePendingOrdersList();
-
-        //App.getAppInstance().setPendingOrdersScreen(this);
-
     }
 
-    // Go back to previous screen
-    public void clickBack(View view) {
-        finish();
-    }
-
+    /**
+     * this helper method retrieves the current CHEF's Orders
+     */
     private void loadPendingOrdersData() {
 
-        // check if the current logged in user is a Chef
-        if (App.getUser() instanceof Chef) {
-            // set meals data to be the list of meals of the Chef
+        // Process: checking if current user is a CHEF
+        if (App.getUser() instanceof Chef) { //is CHEF
+            // Initialization: setting ordersData to the map of orderIDs & Orders
             this.ordersData = ((Chef) App.getUser()).getOrders();
         }
-        else {
+        else { //not a chef -> error-handling
             Log.e("PendingOrdersScreen", "Can't show pending offered; Current logged-in user is not a CHEF");
+
+            // Output
             displayErrorToast("No pending orders available to be displayed!");
         }
 
     }
 
+    /**
+     * this helper method populates the Orders list
+     */
     private void populatePendingOrdersList() {
 
         // Variable Declaration
         this.orders = new ArrayList<Order>();
-
-        // get the meals list
         ListView pendingOrdersList = findViewById(R.id.pendingListView);
-        // get the adapter
+
+        // Initialization: setting the adapter
         pendingOrdersAdapter = new PendingOrdersAdapter(this, R.layout.activity_pending_orders_list_item, this.orders);
 
-        // Attach the adapter to the pending orders listView
+        // Process: attaching the adapter to the ListView
         pendingOrdersList.setAdapter(pendingOrdersAdapter);
 
-        // add data to the adapter
+        // Process: looping through the map of data
         for (Order order: this.ordersData.values()) {
-            pendingOrdersAdapter.add(order);
+            pendingOrdersAdapter.add(order); //adding the orderData to the list
         }
+
     }
 }
