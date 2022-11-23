9e1d73633b1ee80aeeac2e0e564aff45f495e17b
Amy Huang <ahuan106@uottawa.ca>
2022-11-23 13:42:26 -0500

    created pending orders adapter

---
+++ app/src/main/java/com/example/mealer_project/ui/screens/PendingOrdersScreen.java
@@ -0,0 +1,24 @@
+package com.example.mealer_project.ui.screens;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import com.example.mealer_project.R;
+import com.example.mealer_project.app.App;
+import com.example.mealer_project.data.models.Chef;
+import com.example.mealer_project.data.models.Order;
+import com.example.mealer_project.data.models.meals.Meal;
+import com.example.mealer_project.ui.core.UIScreen;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PendingOrdersScreen extends UIScreen {
+
+
+}
