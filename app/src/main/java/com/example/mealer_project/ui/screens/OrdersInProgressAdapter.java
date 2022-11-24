4079026d41aeb7580cb7e6313d23d0118619e292
kkristene3 <kkristene3@gmail.com>
2022-11-24 01:53:26 -0500

    Possibly did the adapter for meals in progress

--- app/src/main/java/com/example/mealer_project/ui/screens/OrdersInProgressAdapter.java
+++ app/src/main/java/com/example/mealer_project/ui/screens/OrdersInProgressAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,13 +13,14 @@ import androidx.annotation.Nullable;
 
 import com.example.mealer_project.R;
 import com.example.mealer_project.app.App;
-import com.example.mealer_project.data.models.Orders;
+import com.example.mealer_project.data.models.Order;
+import com.example.mealer_project.data.models.orders.MealInfo;
 import com.example.mealer_project.data.models.orders.OrderItem;
 
 import java.util.List;
 
 
-public class OrdersInProgressAdapter extends ArrayAdapter<OrderItem> {
+public class OrdersInProgressAdapter extends ArrayAdapter<Order> {
 
 
     /**
@@ -28,7 +30,7 @@ public class OrdersInProgressAdapter extends ArrayAdapter<OrderItem> {
      * @param resource
      * @param objects
      */
-    public OrdersInProgressAdapter(@NonNull Context context, int resource, @NonNull List<OrderItem> objects) {
+    public OrdersInProgressAdapter(@NonNull Context context, int resource, @NonNull List<Order> objects) {
         super(context, resource, objects);
     }
 
@@ -37,7 +39,11 @@ public class OrdersInProgressAdapter extends ArrayAdapter<OrderItem> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
         // get chef's orders in progress
-        Orders order = App.getChef().ORDERS;
+        Order order = getItem(position);
+
+        //
+        String mealNames = "";
+        String quantities = "";
 
 
         // check if an existing view is being reused, otherwise, inflate
@@ -45,8 +51,27 @@ public class OrdersInProgressAdapter extends ArrayAdapter<OrderItem> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.orders_in_progress, parent, false);
         }
 
+        // Process: traversing entire meals map
+        for (MealInfo mI : order.getMeals().values()) {
+
+            mealNames += mI.getName() + "\n";
+            quantities += mI.getQuantity() + "\n";
+
+        }
+
         // populate data
-        ((TextView) convertView.findViewById(R.id.clientText)).setText("");
+        ((TextView) convertView.findViewById(R.id.clientText)).setText(order.getClientInfo().getClientName());
+        ((TextView) convertView.findViewById(R.id.dateForOrder)).setText(order.getOrderDate().toString());
+        ((TextView) convertView.findViewById(R.id.mealNameText)).setText(mealNames);
+        ((TextView) convertView.findViewById(R.id.quantityOfMealInProgress)).setText("Q: " + quantities);
+        ((Button) convertView.findViewById(R.id.doneButton)).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                order.setIsCompleted(true);
+                // also remove the completed order from this screen!
+            }
+        });
 
         return convertView;
     }
