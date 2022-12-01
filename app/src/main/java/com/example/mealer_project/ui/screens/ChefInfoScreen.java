4c5c4645366c0e9ccde1fa211315b65f3c0eeba6
Rahul <Ratre075@uottawa.ca>
2022-12-01 15:16:46 -0500

    Created Chef Info Screen & added its respective screen class

---
+++ app/src/main/java/com/example/mealer_project/ui/screens/ChefInfoScreen.java
@@ -0,0 +1,23 @@
+package com.example.mealer_project.ui.screens;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.example.mealer_project.R;
+import com.example.mealer_project.app.App;
+import com.example.mealer_project.data.handlers.MealHandler;
+import com.example.mealer_project.data.models.meals.Meal;
+import com.example.mealer_project.ui.core.StatefulView;
+import com.example.mealer_project.ui.core.UIScreen;
+import com.example.mealer_project.ui.screens.meals.MealsAdapter;
+
+import java.util.ArrayList;
+
+public class ChefInfoScreen extends UIScreen implements StatefulView {
+}
