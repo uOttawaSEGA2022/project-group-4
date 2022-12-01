e0440fed570ddc55574ffd2fff41bb6387ea38e6
Rahul <Ratre075@uottawa.ca>
2022-12-01 15:45:08 -0500

    Updated id conflict

--- app/src/main/java/com/example/mealer_project/ui/screens/ChefInfoScreen.java
+++ app/src/main/java/com/example/mealer_project/ui/screens/ChefInfoScreen.java
@@ -11,13 +11,40 @@ import android.widget.TextView;
 
 import com.example.mealer_project.R;
 import com.example.mealer_project.app.App;
-import com.example.mealer_project.data.handlers.MealHandler;
 import com.example.mealer_project.data.models.meals.Meal;
 import com.example.mealer_project.ui.core.StatefulView;
 import com.example.mealer_project.ui.core.UIScreen;
-import com.example.mealer_project.ui.screens.meals.MealsAdapter;
-
-import java.util.ArrayList;
 
 public class ChefInfoScreen extends UIScreen implements StatefulView {
+
+    ImageButton backButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // buttons for onClick methods
+        backButton = (ImageButton) findViewById(R.id.button_back5);
+    }
+
+
+    @Override
+    public void updateUI() {
+
+    }
+
+    @Override
+    public void showNextScreen() {
+
+    }
+
+    @Override
+    public void dbOperationSuccessHandler(Object dbOperation, Object payload) {
+
+    }
+
+    @Override
+    public void dbOperationFailureHandler(Object dbOperation, Object payload) {
+
+    }
 }
